475458769acd64de174efc0a2333ceed6348d137
Vaishakh G M <mehendalevgm@gmail.com>
2022-02-21 18:47:28 +0530

    4th Commit

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -26,9 +26,9 @@ public class TypewiseAlertTest
     {
         SampleBattery sampleBattery = BatteryManager.initBattery("sample", CoolingType.PASSIVE_COOLING);
 
-        assertTrue(BatteryManager.inferBreach(41, sampleBattery) == BreachType.TOO_HIGH);
-        assertTrue(BatteryManager.isLowerBreach(41, sampleBattery) == false);
-        assertTrue(BatteryManager.isUpperBreach(41, sampleBattery) == true);
+        assertTrue(BatteryManager.inferBreach(36, sampleBattery) == BreachType.TOO_HIGH);
+        assertTrue(BatteryManager.isLowerBreach(36, sampleBattery) == false);
+        assertTrue(BatteryManager.isUpperBreach(36, sampleBattery) == true);
 
         assertTrue(BatteryManager.inferBreach(-1, sampleBattery) == BreachType.TOO_LOW);
         assertTrue(BatteryManager.isLowerBreach(-1, sampleBattery) == true);
@@ -41,9 +41,9 @@ public class TypewiseAlertTest
     {
         SampleBattery sampleBattery = BatteryManager.initBattery("sample", CoolingType.MED_ACTIVE_COOLING);
 
-        assertTrue(BatteryManager.inferBreach(36, sampleBattery) == BreachType.TOO_HIGH);
-        assertTrue(BatteryManager.isLowerBreach(36, sampleBattery) == false);
-        assertTrue(BatteryManager.isUpperBreach(36, sampleBattery) == true);
+        assertTrue(BatteryManager.inferBreach(41, sampleBattery) == BreachType.TOO_HIGH);
+        assertTrue(BatteryManager.isLowerBreach(41, sampleBattery) == false);
+        assertTrue(BatteryManager.isUpperBreach(41, sampleBattery) == true);
 
         assertTrue(BatteryManager.inferBreach(-1, sampleBattery) == BreachType.TOO_LOW);
         assertTrue(BatteryManager.isLowerBreach(-1, sampleBattery) == true);
